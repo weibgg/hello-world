f722116210dba0affa0da49e5246ff0727352c09
weibgg <weibgg@163.com>
2021-01-12 20:05:40 +0800

    modify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
-      <h2>预祝0713java全体学生前程似锦</h2>
-      <h2>预祝0713java全体学生统统月薪过万</h2>
-      <h2>预祝0713java全体学生统统月薪过两万</h2>
-      <h2>预祝0713java全体学生统统月薪过三万</h2>
-      <h2>预祝0713java全体学生统统年薪过三十万</h2>
-   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+      <h2>央视牛年春晚Logo发布</h2>
+   <img src="http://pics5.baidu.com/feed/9213b07eca8065384d9be26345e74343ad34820b.jpeg?token=fe753cdc861f2fc573ef2e95718c92d7&s=1ED7CA5E48104CC61E17AEA80300701E"/>
   </div>
 </template>
 
