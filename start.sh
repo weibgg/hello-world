b47ef848782b6d7c4a11b9a8810e5db34a9cd776
weibgg <weibgg@163.com>
2021-01-12 20:14:05 +0800

    modify App.vue

--- start.sh
+++ start.sh
@@ -4,13 +4,12 @@ git pull
 # 安装依赖和打包
 npm install --registry=https://registry.npm.taobao.org/ && npm run build
 
-# 删除容器
-docker rm -f demo1 &> /dev/null
+# 删除容器(nginx是自己启动的nginx的镜像名称对应docker ps结果中的NAMES字段)
+docker rm -f nginx &> /dev/null
 
-#启动容器
+# 启动容器(次写法后此脚本只能放在/opt/apps/hello-world中执行否则找不到项目中的dist文件夹)
 docker run -d --restart=on-failure:5\
     -p 8080:80 \
     -v $PWD/dist:/usr/share/nginx/html \
-    --name demo1 nginx
-
+    --name nginx nginx
 
